08093accc2fd804a2f39c94a9d5044699a004b84
ZhuXiaoran07 <zhuxrsandra@163.com>
2017-05-30 23:22:24 -0500

    Delete Chained-Hash-Insert.c

--- CH-11/Chained-Hash-Insert.c
+++
@@ -1,9 +0,0 @@
-//P258
-CHAINEDHASINSERT(T,x)
-insert x at the head of list T[h(x.key)]
-
-CHAINEDHASSEARCH(T,k)
-search for an element with key k in list T[h(k)]
-
-CHAINEDHASDELETE(T,x)
-delete x from the list T[h(x.key)]
