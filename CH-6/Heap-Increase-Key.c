6d35e55bea79ee67be5e1619353a009d15e0a248
ZhuXiaoran07 <zhuxrsandra@163.com>
2017-05-30 23:30:53 -0500

    Delete Heap-Increase-Key.c

--- CH-6/Heap-Increase-Key.c
+++
@@ -1,8 +0,0 @@
-//P164
-HEAPINCREASEKEY(A,i,key)
- if key<A[i]
- 	error "new key is smaller than current key"
- A[i]=key
- while i>1 and A[PARENT(i)]<A[i]{
-	exchange A[i] with A[PARENT(i)]
-	i=PARENT(i)}
